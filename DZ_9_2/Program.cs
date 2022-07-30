04d39fb17d5de26afc1a5d317797716887b528bb
maryakovalenko <pomashka18061992@gmail.com>
2022-07-30 12:59:28 +0300

    DZ_9

---
+++ DZ_9_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Знакомство с языками программирования домашняя работа к уроку 9. Как не нужно писать код.
+Console.WriteLine("Задача № 66");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+PrintSumm(m, n, temp=0);
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
